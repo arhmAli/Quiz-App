8d80d67d697f57aebe0cb2ce1bc97e0e93483463
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2022-10-07 10:32:38 +0500

    added the questions

---
+++ db.js
@@ -0,0 +1,155 @@
+const moreQuestions = [
+    {
+        question: " Among the given statements, which statement defines closures in JavaScript?",
+        option_a: " JavaScript is a function that is enclosed with references to its inner function scope",
+        option_b: " JavaScript is a function that is enclosed with references to its lexical environment",
+        option_c: " JavaScript is a function that is enclosed with the object to its inner function scope",
+        option_d: " None of the mentioned",
+        correctAnswer: "option_b"
+    }
+    ,
+    {
+        question: " Among the given statements, which statement defines closures in JavaScript?",
+        option_a: " JavaScript is a function that is enclosed with references to its inner function scope",
+        option_b: " JavaScript is a function that is enclosed with references to its lexical environment",
+        option_c: " JavaScript is a function that is enclosed with the object to its inner function scope",
+        option_d: " None of the mentioned",
+        correctAnswer: "option_b"
+    }
+    ,
+    {
+        question: " Among the given statements, which statement defines closures in JavaScript?",
+        option_a: " JavaScript is a function that is enclosed with references to its inner function scope",
+        option_b: " JavaScript is a function that is enclosed with references to its lexical environment",
+        option_c: " JavaScript is a function that is enclosed with the object to its inner function scope",
+        option_d: " None of the mentioned",
+        correctAnswer: "option_b"
+    }
+    ,
+    {
+        question: " Among the given statements, which statement defines closures in JavaScript?",
+        option_a: " JavaScript is a function that is enclosed with references to its inner function scope",
+        option_b: " JavaScript is a function that is enclosed with references to its lexical environment",
+        option_c: " JavaScript is a function that is enclosed with the object to its inner function scope",
+        option_d: " None of the mentioned",
+        correctAnswer: "option_b"
+    }
+    ,
+    {
+        question: " Among the given statements, which statement defines closures in JavaScript?",
+        option_a: " JavaScript is a function that is enclosed with references to its inner function scope",
+        option_b: " JavaScript is a function that is enclosed with references to its lexical environment",
+        option_c: " JavaScript is a function that is enclosed with the object to its inner function scope",
+        option_d: " None of the mentioned",
+        correctAnswer: "option_b"
+    }
+    ,
+    {
+        question: " Among the given statements, which statement defines closures in JavaScript?",
+        option_a: " JavaScript is a function that is enclosed with references to its inner function scope",
+        option_b: " JavaScript is a function that is enclosed with references to its lexical environment",
+        option_c: " JavaScript is a function that is enclosed with the object to its inner function scope",
+        option_d: " None of the mentioned",
+        correctAnswer: "option_b"
+    }
+    ,
+    {
+        question: " Among the given statements, which statement defines closures in JavaScript?",
+        option_a: " JavaScript is a function that is enclosed with references to its inner function scope",
+        option_b: " JavaScript is a function that is enclosed with references to its lexical environment",
+        option_c: " JavaScript is a function that is enclosed with the object to its inner function scope",
+        option_d: " None of the mentioned",
+        correctAnswer: "option_b"
+    }
+    ,
+    {
+        question: " Among the given statements, which statement defines closures in JavaScript?",
+        option_a: " JavaScript is a function that is enclosed with references to its inner function scope",
+        option_b: " JavaScript is a function that is enclosed with references to its lexical environment",
+        option_c: " JavaScript is a function that is enclosed with the object to its inner function scope",
+        option_d: " None of the mentioned",
+        correctAnswer: "option_b"
+    }
+    ,
+    {
+        question: " Among the given statements, which statement defines closures in JavaScript?",
+        option_a: " JavaScript is a function that is enclosed with references to its inner function scope",
+        option_b: " JavaScript is a function that is enclosed with references to its lexical environment",
+        option_c: " JavaScript is a function that is enclosed with the object to its inner function scope",
+        option_d: " None of the mentioned",
+        correctAnswer: "option_b"
+    }
+    ,
+    {
+        question: " Among the given statements, which statement defines closures in JavaScript?",
+        option_a: " JavaScript is a function that is enclosed with references to its inner function scope",
+        option_b: " JavaScript is a function that is enclosed with references to its lexical environment",
+        option_c: " JavaScript is a function that is enclosed with the object to its inner function scope",
+        option_d: " None of the mentioned",
+        correctAnswer: "option_b"
+    }
+    ,
+    {
+        question: " Among the given statements, which statement defines closures in JavaScript?",
+        option_a: " JavaScript is a function that is enclosed with references to its inner function scope",
+        option_b: " JavaScript is a function that is enclosed with references to its lexical environment",
+        option_c: " JavaScript is a function that is enclosed with the object to its inner function scope",
+        option_d: " None of the mentioned",
+        correctAnswer: "option_b"
+    }
+    ,
+    {
+        question: " Among the given statements, which statement defines closures in JavaScript?",
+        option_a: " JavaScript is a function that is enclosed with references to its inner function scope",
+        option_b: " JavaScript is a function that is enclosed with references to its lexical environment",
+        option_c: " JavaScript is a function that is enclosed with the object to its inner function scope",
+        option_d: " None of the mentioned",
+        correctAnswer: "option_b"
+    }
+    ,
+    {
+        question: " Among the given statements, which statement defines closures in JavaScript?",
+        option_a: " JavaScript is a function that is enclosed with references to its inner function scope",
+        option_b: " JavaScript is a function that is enclosed with references to its lexical environment",
+        option_c: " JavaScript is a function that is enclosed with the object to its inner function scope",
+        option_d: " None of the mentioned",
+        correctAnswer: "option_b"
+    }
+    ,
+    {
+        question: " Among the given statements, which statement defines closures in JavaScript?",
+        option_a: " JavaScript is a function that is enclosed with references to its inner function scope",
+        option_b: " JavaScript is a function that is enclosed with references to its lexical environment",
+        option_c: " JavaScript is a function that is enclosed with the object to its inner function scope",
+        option_d: " None of the mentioned",
+        correctAnswer: "option_b"
+    }
+    ,
+    {
+        question: " Among the given statements, which statement defines closures in JavaScript?",
+        option_a: " JavaScript is a function that is enclosed with references to its inner function scope",
+        option_b: " JavaScript is a function that is enclosed with references to its lexical environment",
+        option_c: " JavaScript is a function that is enclosed with the object to its inner function scope",
+        option_d: " None of the mentioned",
+        correctAnswer: "option_b"
+    }
+    ,
+    {
+        question: " Among the given statements, which statement defines closures in JavaScript?",
+        option_a: " JavaScript is a function that is enclosed with references to its inner function scope",
+        option_b: " JavaScript is a function that is enclosed with references to its lexical environment",
+        option_c: " JavaScript is a function that is enclosed with the object to its inner function scope",
+        option_d: " None of the mentioned",
+        correctAnswer: "option_b"
+    }
+    ,
+    {
+        question: " Among the given statements, which statement defines closures in JavaScript?",
+        option_a: " JavaScript is a function that is enclosed with references to its inner function scope",
+        option_b: " JavaScript is a function that is enclosed with references to its lexical environment",
+        option_c: " JavaScript is a function that is enclosed with the object to its inner function scope",
+        option_d: " None of the mentioned",
+        correctAnswer: "option_b"
+    }
+]
+export default moreQuestions;
